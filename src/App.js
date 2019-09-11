0a4ae81e2f2a5c9cb0a044befab3d613fc39b6d7
andres-martin <andresmartin266@gmail.com>
2019-09-10 23:40:26 -0500

    [feature] repeats the text typed in the text field

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { content: "" }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" value={this.state.content} onChange={this.updateContent.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.content}</p>
       </div>
     );
   }
+
+  // method that updates the state.content value
+  updateContent(event) {
+    this.setState({
+      content: event.target.value
+    });
+
+  }
+
 }
 
 export default App;
